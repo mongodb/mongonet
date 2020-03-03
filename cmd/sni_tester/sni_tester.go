e2d5b37a7e7bdf2e3f41a205bda92541d10d0e3d
TomerYakir <tomeryakir@gmail.com>
2020-03-03 10:57:25 +0200

    support unacked writes (#37)

--- cmd/sni_tester/sni_tester.go
+++ cmd/sni_tester/sni_tester.go
@@ -35,33 +35,33 @@ func (myi *MyInterceptor) sniResponse() mongonet.SimpleBSON {
 	return raw
 }
 
-func (myi *MyInterceptor) InterceptClientToMongo(m mongonet.Message) (mongonet.Message, mongonet.ResponseInterceptor, error) {
+func (myi *MyInterceptor) InterceptClientToMongo(m mongonet.Message) (mongonet.Message, mongonet.ResponseInterceptor, bool, error) {
 	switch mm := m.(type) {
 	case *mongonet.QueryMessage:
 		if !mongonet.NamespaceIsCommand(mm.Namespace) {
-			return m, nil, nil
+			return m, nil, true, nil
 		}
 
 		query, err := mm.Query.ToBSOND()
 		if err != nil || len(query) == 0 {
 			// let mongod handle error message
-			return m, nil, nil
+			return m, nil, true, nil
 		}
 
 		cmdName := query[0].Key
 		if cmdName != "sni" {
-			return m, nil, nil
+			return m, nil, true, nil
 		}
 
-		return nil, nil, newSNIError(myi.ps.RespondToCommand(mm, myi.sniResponse()))
+		return nil, nil, true, newSNIError(myi.ps.RespondToCommand(mm, myi.sniResponse()))
 	case *mongonet.CommandMessage:
 		if mm.CmdName != "sni" {
-			return mm, nil, nil
+			return mm, nil, true, nil
 		}
-		return nil, nil, newSNIError(myi.ps.RespondToCommand(mm, myi.sniResponse()))
+		return nil, nil, true, newSNIError(myi.ps.RespondToCommand(mm, myi.sniResponse()))
 	}
 
-	return m, nil, nil
+	return m, nil, true, nil
 }
 
 func (myi *MyInterceptor) Close() {
