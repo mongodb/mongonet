d2da70461b138f6b8bfd55e64d694169f25236c2
Tim Olsen <tim.olsen@mongodb.com>
2021-06-22 12:31:58 -0400

    Revert "revert server.go"
    This reverts commit e9bdfbf1bd43c271446072fd3912953cc8da3fe7.

--- server.go
+++ server.go
@@ -164,9 +164,11 @@ func (s *Server) Run() error {
 		s.cancelCtx()
 
 		// wait for all sessions to end
-		s.logger.Logf(slogger.WARN, "waiting for sessions to close...")
-		s.sessionManager.sessionWG.Wait()
-		s.logger.Logf(slogger.WARN, "done")
+		if s.sessionManager != nil {
+			s.logger.Logf(slogger.WARN, "waiting for sessions to close...")
+			s.sessionManager.sessionWG.Wait()
+			s.logger.Logf(slogger.WARN, "done")
+		}
 
 		close(s.doneChan)
 	}()
